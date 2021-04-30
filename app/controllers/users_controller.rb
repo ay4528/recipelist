d7a5218bae5fc564f4ee392e7bc0b3259014f1df
ayu <ayuabc4528@gmail.com>
2021-04-30 14:29:45 +0000

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,31 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show,]
+  
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "プロフィールを更新しました。"
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :profile, :profile_image)
   end
 end

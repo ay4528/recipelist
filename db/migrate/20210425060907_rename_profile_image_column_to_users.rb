696067649df82821fab1c86ad79c6e279459febf
ayu <ayuabc4528@gmail.com>
2021-05-06 14:21:33 +0000

    delete migration

---
+++ db/migrate/20210425060907_rename_profile_image_column_to_users.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# RenameProfileImageColumnToUsers
+class RenameProfileImageColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :users, :profile_image, :profile_image_id
+  end
+end
